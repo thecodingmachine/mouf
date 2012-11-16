cf901d54853b345112b01d932bd1c37d8e1b394d
David Négrier <david@davidtcm>
2012-11-16 15:13:29 +0100

    Fixing imports and install process validator

--- src/direct/install_process_validator.php
+++ src/direct/install_process_validator.php
@@ -1,4 +1,6 @@
 <?php
+use Mouf\MoufManager;
+
 /*
  * This file is part of the Mouf core package.
  *
@@ -9,28 +11,33 @@
  */
  
 // This validator checks that no installation step is pending.
+use Mouf\MoufUtils;
 
 
 ini_set('display_errors', 1);
 // Add E_ERROR to error reporting it it is not already set
 error_reporting(E_ERROR | error_reporting());
 
-/*if (!isset($_REQUEST["selfedit"]) || $_REQUEST["selfedit"]!="true") {
-	require_once '../../Mouf.php';
-	$selfEdit = "false";
-} else {*/
-	require_once '../../MoufComponents.php';
+require_once '../../mouf/Mouf.php';
+
+if (!isset($_REQUEST["selfedit"]) || $_REQUEST["selfedit"]!="true") {
+	$selfEdit = false;
+} else {
+	$selfEdit = true;
+}
+
+/*	require_once '../../MoufComponents.php';
 	require_once '../MoufManager.php';
 	MoufManager::initMoufManager();
 	require_once '../../MoufUniversalParameters.php';
 	MoufManager::switchToHidden();
 	require_once '../MoufAdmin.php';
-	$selfEdit = "true";
-//}
+	$selfEdit = "true";*/
 
 // Note: checking rights is done after loading the required files because we need to open the session
 // and only after can we check if it was not loaded before loading it ourselves...
-require_once 'utils/check_rights.php';
+//require_once 'utils/check_rights.php';
+MoufUtils::checkRights();
 
 $moufManager = MoufManager::getMoufManager();
 
@@ -45,7 +52,7 @@ if (!$multiStepActionService->hasRemainingAction()) {
 	$jsonObj['html'] = "No pending install actions to execute.";
 } else {
 	$jsonObj['code'] = "warn";
-	$jsonObj['html'] = "An installation process did not complete. Please <a href='".ROOT_URL."mouf/install/?selfedit=".$selfEdit."'>resume the install process</a>.";
+	$jsonObj['html'] = "An installation process did not complete. Please <a href='".ROOT_URL."install/?selfedit=".json_encode($selfEdit)."'>resume the install process</a>.";
 }
 
 echo json_encode($jsonObj);
