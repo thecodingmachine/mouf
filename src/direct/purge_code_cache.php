fe304411912a8a5f492a78651a87531c467e2364
David Négrier <david@davidtcm>
2013-01-15 20:47:36 +0100

    Adding 'purge code cache' button

---
+++ src/direct/purge_code_cache.php
@@ -0,0 +1,26 @@
+<?php
+use Mouf\MoufCache;
+use Mouf\MoufUtils;
+
+// This file purges all the code cache from Mouf.
+
+// Disable output buffering
+while (ob_get_level() != 0) {
+	ob_end_clean();
+}
+
+ini_set('display_errors', 1);
+// Add E_ERROR to error reporting if it is not already set
+error_reporting(E_ERROR | error_reporting());
+
+require_once '../../../../../mouf/Mouf.php';
+
+// Note: checking rights is done after loading the required files because we need to open the session
+// and only after can we check if it was not loaded before loading it ourselves...
+MoufUtils::checkRights();
+
+$moufCache = new MoufCache();
+$moufCache->purgeAll();
+
+exit;
+?>
