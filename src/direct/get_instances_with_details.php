e888438822f700cb081483f74bfc1e1155c0eb87
Pierre Vaidie <pierre.vaidie@gmail.com>
2013-11-27 11:29:52 +0100

    Fix issue with UTF8 mall formed character when get classes -> Add error message

--- src/direct/get_instances_with_details.php
+++ src/direct/get_instances_with_details.php
@@ -90,6 +90,7 @@ foreach ($classList as $childClassName) {
 
 // List of all classes that might be relevant
 $response["classes"] = $classArray;
+
 // List of all classes that are a subclass of the main class / interface
 $response["childrenClasses"] = $childClassArray;
 
@@ -103,8 +104,44 @@ if ($encode == "php") {
 } elseif ($encode == "json") {
 	header("Content-type: application/json");
 	echo json_encode($response);
+	
+	if (!(json_last_error() == JSON_ERROR_NONE)) {
+		checkJsonEncode($instanceArray);
+		checkJsonEncode($classArray);
+		checkJsonEncode($childClassArray);
+	}
 } else {
 	echo "invalid encode parameter";
+}
+
+function checkJsonEncode($array) {
+	foreach($array as $key =>$value) {
+		json_encode($value);
+			
+		if (!(json_last_error() == JSON_ERROR_NONE)) {
+			switch (json_last_error()) {
+				case JSON_ERROR_DEPTH:
+					echo $key.' - Maximum stack depth exceeded';
+					break;
+				case JSON_ERROR_STATE_MISMATCH:
+					echo $key.' - Underflow or the modes mismatch';
+					break;
+				case JSON_ERROR_CTRL_CHAR:
+					echo $key.' - Unexpected control character found';
+					break;
+				case JSON_ERROR_SYNTAX:
+					echo $key.' - Syntax error, malformed JSON';
+					break;
+				case JSON_ERROR_UTF8:
+					echo $key.' - Malformed UTF-8 characters, possibly incorrectly encoded';
+						
+					break;
+				default:
+					echo $key.' - Unknown error';
+					break;
+			}
+		}
+	}
 }
 
 ?>
