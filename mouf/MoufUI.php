df4bbf798152971a7490f528b13cfa02fd95a9f3
David Négrier <d.negrier@thecodingmachine.com>
2014-06-04 10:33:52 +0200

    Updating dependencies

--- mouf/MoufUI.php
+++ mouf/MoufUI.php
@@ -5,6 +5,7 @@
 
 // Files declared in the extra:mouf:adminRequire section.
 require_once __DIR__.'/../vendor/mouf/utils.cache.cache-interface/CacheAdmin.php';
+require_once __DIR__.'/../vendor/mouf/html.utils.weblibrarymanager/src/WebLibraryAdmin.php';
 require_once __DIR__.'/../vendor/mouf/utils.i18n.fine/src/FineAdmin.php';
 require_once __DIR__.'/../vendor/mouf/mvc.splash-common/src/SplashCommonAdmin.php';
 require_once __DIR__.'/../vendor/mouf/mvc.splash/src/SplashAdmin.php';
