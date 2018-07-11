0297285ef1287ed4db41104c2b5a877529e668a9
David Négrier <d.negrier@thecodingmachine.com>
2018-07-11 13:54:59 +0200

    Disable UI via environment variable

--- src/splash.php
+++ src/splash.php
@@ -7,7 +7,18 @@
  * For the full copyright and license information, please view the LICENSE.txt
  * file that was distributed with this source code.
  */
- 
+
+$moufUI = getenv('MOUF_UI');
+if ($moufUI !== false) {
+    $moufUI = (bool) $moufUI;
+    if (!$moufUI) {
+        header('HTTP/1.1 403 Forbidden');
+        echo 'Error! Access to Mouf UI is forbidden on this environment (env variable MOUF_UI is set to 0)';
+        exit;
+    }
+}
+unset($moufUI);
+
 // Let's load the Mouf file, and the MoufAdmin file.
 // The MoufAdmin will replace the Mouf configuration file.
 if (file_exists(dirname(__FILE__).'/../MoufComponents.php')) {
